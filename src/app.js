0372ca1bb940ef6ef746d6d0d5aadf6a0881d3b8
Noel Espino Córdova <noelespinocordova@tutanota.com>
2020-09-07 20:57:31 -0500

    Add cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 
 const router = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(morgan('dev'));
 
 app.use(router);
